a674ba5e5c525261321a332b4a5a79c1cf22449c
Pedro <pedrorubiorigo@gmail.com>
2025-03-06 08:52:42 +0100

    colocar bombas

--- src/Tablero.java
+++ src/Tablero.java
@@ -2,6 +2,7 @@ public class Tablero {
     private final int filas = 10;
     private final int columnas = 10;
     private static Casilla[][] tablero;
+    private static int numero_minas = 20;
 
 
     public Tablero (){
@@ -23,4 +24,22 @@ public class Tablero {
     public int getFilas() {
         return filas;
     }
+
+    private void colocar_bombas(){
+
+        for(int i = 0; i < numero_minas;i++){
+            int fila = (int) (Math.floor(Math.random()*(10+1)));
+            int columna = (int) (Math.floor(Math.random()*(10+1)));
+            if(!tablero[columna][fila].isBomba() ){
+                tablero[columna][fila].setBomba(true);
+            } else {
+                i--;
+            }
+        }
+    }
+
+    private void contar_bombas(){
+
+    }
+
 }
